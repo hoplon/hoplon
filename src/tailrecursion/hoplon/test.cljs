d045046e259a703fdc3f8474d5f67a6ede4a9fe9
Micha Niskin <asdf@ubergibson.com>
2013-05-28 18:45:01 -0400

    Add test namespace; go back to cljs proper

---
+++ src/tailrecursion/hoplon/test.cljs
@@ -0,0 +1,14 @@
+(ns tailrecursion.hoplon.test)
+
+(defn ^:export setup! []
+  (set! cljs.core/*print-fn*
+        (if (undefined? (aget js/window "dump"))
+          ;; phantomjs
+          #(.apply (.-log js/console)
+                   (.-console js/window)
+                   (apply array %&))
+          ;; firefox
+          #(.apply (aget js/window "dump")
+                   js/window
+                   (apply array %&)))))
+
