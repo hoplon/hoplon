00837a809a6e7d9f47291d1425d1b251a82e1bb4
Micha Niskin <asdf@ubergibson.com>
2013-06-18 11:45:48 -0400

    Don't do anything with cljs files in deps
    The cljs compiler picks up cljs files from the classpath so this is now
    unnecessary.

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
                        :url "http://www.eclipse.org/legal/epl-v10.html"}
   :manifest           {"hoplon-provides"            "hoplon"}
   :dependencies       [[org.clojure/clojure         "1.5.1"]
+                       [org.clojure/clojurescript   "0.0-1820"]
                        [tailrecursion/javelin       "1.0.0-SNAPSHOT"]
                        [fipp                        "0.3.0-SNAPSHOT"]
                        [digest                      "1.3.0"]
                        [criterium                   "0.3.0"]
                        [clj-tagsoup                 "0.3.0"]
-                       [org.clojure/core.incubator  "0.1.2"]
-                       [org.clojure/clojurescript   "0.0-1552"]])
+                       [org.clojure/core.incubator  "0.1.2"]])
