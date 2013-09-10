5cbf108ae171031dd982497687a6d2ee8498f1e6
Micha Niskin <asdf@ubergibson.com>
2013-09-10 02:33:05 -0400

    clean tmpdirs prior to building; add syncs; use :static from boot config

--- src/tailrecursion/hoplon/boot.clj
+++ src/tailrecursion/hoplon/boot.clj
@@ -1,24 +1,28 @@
 (ns tailrecursion.hoplon.boot
   (:require
     [tailrecursion.boot.task                  :as t]
-    [tailrecursion.boot.task.util.file        :as f]
+    [tailrecursion.boot.file                  :as f]
     [tailrecursion.boot.core                  :refer [deftask]]
     [clojure.java.io                          :refer [file make-parents]]
-    [tailrecursion.boot.tmpregistry           :refer [mk! mkdir!]]
+    [tailrecursion.boot.tmpregistry           :refer [mk! mkdir! add-sync!]]
     [tailrecursion.hoplon.compiler.compiler   :refer [compile-dirs]]))
 
 (deftask hoplon
   "Build Hoplon web application."
-  [boot & {:keys [static-dir cljs-opts]}]
-  (let [{:keys [public src-paths system]} @boot
+  [boot & [cljs-opts]]
+  (let [{:keys [public src-paths system static]} @boot
         tmp         (:tmpregistry system)
-        static-dir  (doto (file (or static-dir "static")) (.mkdirs)) 
+        static-dir  (file static)
         src-paths   (map file src-paths)
         cljs-tmp    (mkdir! tmp ::cljs-tmp)
         public-tmp  (mkdir! tmp ::public-tmp)
         main-js     (file public-tmp "main.js")]
+    (add-sync! tmp public [public-tmp static-dir])
     (swap! boot update-in [:src-paths] conj (.getPath cljs-tmp))
     (comp
-      #((t/pass-thru-wrap compile-dirs) % main-js src-paths cljs-tmp public-tmp)
-      (t/cljs boot :output-to main-js :opts cljs-opts)
-      (t/pre-task boot :process #(do (f/sync :time public public-tmp static-dir) %)))))
+      #(fn [event]
+         (mkdir! tmp ::cljs-tmp)
+         (mkdir! tmp ::public-tmp)
+         (compile-dirs main-js src-paths cljs-tmp public-tmp)
+         (% event))
+      (t/cljs boot :output-to main-js :opts cljs-opts))))
