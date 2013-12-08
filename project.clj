b529b9d10fa6c8a93e60ed5872ab736065a29d52
Micha Niskin <asdf@ubergibson.com>
2013-12-07 21:42:53 -0500

    rm boot.task dep

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license            {:name "Eclipse Public License"
                        :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies       [[tailrecursion/javelin       "2.4.0"]
-                       [tailrecursion/boot.task     "0.1.3"]
                        [tailrecursion/castra        "0.1.1"]
                        [clj-tagsoup                 "0.3.0"]
                        [org.clojure/core.incubator  "0.1.2"]
