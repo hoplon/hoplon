097abb63e96fcc6da420e13ad16df4befebb1a89
Michael Berg <michael.berg@zalf.de>
2013-12-24 14:54:15 +0100

    temporarily changed version number to 4.0.7
    ... to have lein install create a new version which can be distinguished
    from original 4.0.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tailrecursion/hoplon "4.0.7"
+(defproject tailrecursion/hoplon "4.0.8"
   :description        "Hoplon web development environment."
   :url                "http://github.com/tailrecursion/hoplon"
   :license            {:name "Eclipse Public License"
@@ -8,3 +8,6 @@
                        [clj-tagsoup                 "0.3.0"]
                        [org.clojure/core.incubator  "0.1.2"]
                        [clj-webdriver               "0.6.0"]])
+
+
+
