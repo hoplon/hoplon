f0f898d41b9897bc72735d2cedd8a8cfd3d14c72
Micha Niskin <micha.niskin@gmail.com>
2014-01-22 12:20:50 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tailrecursion/hoplon "4.0.11"
+(defproject tailrecursion/hoplon "4.0.12"
   :description        "Hoplon web development environment."
   :url                "http://github.com/tailrecursion/hoplon"
   :license            {:name "Eclipse Public License"
