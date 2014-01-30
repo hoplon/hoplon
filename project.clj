440c2972cd067abee847a8282ec20ce1085fa2f4
Micha Niskin <micha.niskin@gmail.com>
2014-01-30 13:31:48 -0500

    Update castra dep

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
                        :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins            [[lein-marginalia             "0.7.1"]]
   :dependencies       [[tailrecursion/javelin       "3.1.0"]
-                       [tailrecursion/castra        "0.1.1"]
+                       [tailrecursion/castra        "1.0.0"]
                        [clj-tagsoup                 "0.3.0"]
                        [org.clojure/core.incubator  "0.1.2"]])
