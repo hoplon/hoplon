ed9c12bc54998928313bdc659e569435e890d755
Micha Niskin <micha.niskin@gmail.com>
2014-02-14 10:51:35 -0500

    Fix typo in `boot.core` dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject tailrecursion/hoplon "5.1.4"
+(defproject tailrecursion/hoplon "5.1.5"
   :description        "Hoplon web development environment."
   :url                "http://github.com/tailrecursion/hoplon"
   :license            {:name "Eclipse Public License"
                        :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins            [[lein-marginalia             "0.7.1"]]
-  :dependencies       [[tailrecursion/boot.core     "2.1.0"]
+  :dependencies       [[tailrecursion/boot.core     "2.2.0"]
                        [tailrecursion/javelin       "3.1.0"]
                        [tailrecursion/castra        "1.0.1"]
                        [clj-tagsoup                 "0.3.0"]
