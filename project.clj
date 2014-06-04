d21b7348f92ab22f9bdb5d092fc58111bd7939a5
Micha Niskin <micha.niskin@gmail.com>
2014-06-04 15:50:10 -0400

    5.10.4 -- Update Castra dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tailrecursion/hoplon "5.10.3"
+(defproject tailrecursion/hoplon "5.10.4"
   :description  "Hoplon web development environment."
   :url          "http://github.com/tailrecursion/hoplon"
   :license      {:name "Eclipse Public License"
@@ -6,6 +6,6 @@
   :plugins      [[lein-marginalia            "0.7.1"]]
   :dependencies [[io.hoplon.vendor/jquery    "1.8.2-0"]
                  [tailrecursion/javelin      "3.3.0"]
-                 [tailrecursion/castra       "1.2.0"]
+                 [tailrecursion/castra       "1.3.0"]
                  [clj-tagsoup                "0.3.0"]
                  [org.clojure/core.incubator "0.1.2"]])
