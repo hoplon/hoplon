2eed93b1a4e8f60f663b3e0cb0a9f531e16a700b
Micha Niskin <micha.niskin@gmail.com>
2014-06-06 18:42:26 -0400

    Add jquery selectize to contrib

---
+++ contrib/jquery.selectize/project.clj
@@ -0,0 +1,8 @@
+(defproject io.hoplon/jquery.selectize "0.1.0"
+  :description  "Hoplon wrapper for the jQuery selectize plugin."
+  :url          "http://github.com/tailrecursion/hoplon/tree/master/contrib/jquery.selectize"
+  :license      {:name "Eclipse Public License"
+                 :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [tailrecursion/hoplon "5.10.3"]
+                 [io.hoplon.vendor/jquery.selectize "0.8.5-0"]])
