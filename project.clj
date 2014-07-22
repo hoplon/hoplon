deaec149a76637296c5cb9f27fccbc292466bdd4
Alan Dipert <alan@dipert.org>
2014-07-22 16:42:32 -0400

    5.10.14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tailrecursion/hoplon "5.10.13"
+(defproject tailrecursion/hoplon "5.10.14"
   :description  "Hoplon web development environment."
   :url          "http://github.com/tailrecursion/hoplon"
   :license      {:name "Eclipse Public License"
