14e0753da6c29a06869a3f32c90ed673b7ed7b10
Micha Niskin <micha.niskin@gmail.com>
2014-07-29 14:37:14 -0400

    5.10.16 -- Update castra dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tailrecursion/hoplon "5.10.15"
+(defproject tailrecursion/hoplon "5.10.16"
   :description  "Hoplon web development environment."
   :url          "http://github.com/tailrecursion/hoplon"
   :license      {:name "Eclipse Public License"
