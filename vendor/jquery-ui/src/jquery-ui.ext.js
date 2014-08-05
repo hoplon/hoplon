f5e4b44f54968142d65d0ab1319fe2a4f25cc299
mynomoto <mynomoto@gmail.com>
2014-08-05 14:16:50 -0300

    Add jquery-ui to vendor

---
+++ vendor/jquery-ui/src/jquery-ui.ext.js
@@ -0,0 +1,38 @@
+jQuery.prototype.effect = function() {};
+jQuery.prototype.switchClass = function() {};
+
+jQuery.prototype.draggable = function() {};
+jQuery.prototype.droppable = function() {};
+jQuery.prototype.resizable = function() {};
+jQuery.prototype.selectable = function() {};
+jQuery.prototype.sortable = function() {};
+
+jQuery.prototype.disableSelection = function() {};
+jQuery.prototype.enableSelection = function() {};
+jQuery.prototype.removeUniqueId = function() {};
+jQuery.prototype.uniqueId = function() {};
+jQuery.prototype.scrollParent = function() {};
+
+jQuery.prototype.accordion = function() {};
+jQuery.prototype.autocomplete = function() {};
+jQuery.prototype.button = function() {};
+jQuery.prototype.datepicker = function() {};
+jQuery.prototype.datepicker.setDefaults = function() {};
+jQuery.prototype.datepicker.formatDate = function() {};
+jQuery.prototype.datepicker.parseDate = function() {};
+jQuery.prototype.datepicker.iso8601Week = function() {};
+jQuery.prototype.datepicker.noWeekends = function() {};
+jQuery.prototype.datepicker.regional = {};
+jQuery.prototype.dialog = function() {};
+jQuery.prototype.menu = function() {};
+jQuery.prototype.progressbar = function() {};
+jQuery.prototype.selectmenu = function() {};
+jQuery.prototype.slider = function() {};
+jQuery.prototype.spinner = function() {};
+jQuery.prototype.tabs = function() {};
+jQuery.prototype.tooltip = function() {};
+
+jQuery.prototype.widget = function() {};
+jQuery.prototype.widget.bridge = function() {};
+
+jQuery.prototype.ui.keyCode = {};
