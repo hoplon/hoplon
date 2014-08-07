35e625997ee733a4ce04f4b86cac28066167181b
Micha Niskin <micha.niskin@gmail.com>
2014-08-07 18:49:29 -0400

    5.10.19 -- Update javelin dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tailrecursion/hoplon "5.10.18"
+(defproject tailrecursion/hoplon "5.10.19"
   :description  "Hoplon web development environment."
   :url          "http://github.com/tailrecursion/hoplon"
   :license      {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :plugins      [[lein-marginalia            "0.7.1"]]
   :dependencies [[io.hoplon.vendor/jquery    "1.8.2-0"]
                  [org.clojure/tools.reader   "0.8.5"]
-                 [tailrecursion/javelin      "3.4.0"]
+                 [tailrecursion/javelin      "3.5.0"]
                  [tailrecursion/castra       "2.2.1"]
                  [clj-tagsoup                "0.3.0"]
                  [org.clojure/core.incubator "0.1.2"]
