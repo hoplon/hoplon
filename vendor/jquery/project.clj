7c1077f38d93b1450596803fc1441c8c3aef19d7
mynomoto <mynomoto@gmail.com>
2014-09-21 13:38:18 -0300

    vendor/jquery 2.1.1-0

--- vendor/jquery/project.clj
+++ vendor/jquery/project.clj
@@ -1,4 +1,4 @@
-(defproject io.hoplon.vendor/jquery "1.11.1-0"
+(defproject io.hoplon.vendor/jquery "2.1.1-0"
   :description  "The Write Less, Do More, JavaScript Library."
   :url          "http://jquery.com/"
   :license      {:name "MIT" :url "http://opensource.org/licenses/MIT"}
