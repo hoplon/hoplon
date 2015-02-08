7d14a57c020e360554031ce1e479ef1bb618bfac
Micha Niskin <micha.niskin@gmail.com>
2015-02-07 19:09:32 -0500

    Paste in strint to eliminate dependency on core.incubator

--- src/tailrecursion/hoplon.clj
+++ src/tailrecursion/hoplon.clj
@@ -8,9 +8,7 @@
 
 (ns tailrecursion.hoplon
   (:refer-clojure :exclude [subs name])
-  (:require
-    [clojure.walk         :as walk]
-    [clojure.core.strint  :as strint]))
+  (:require [clojure.walk :as walk]))
 
 (create-ns 'js)
 (create-ns 'tailrecursion.javelin)
@@ -37,8 +35,37 @@
           (keyword? head) [tag (into {} (mkkw args)) (drkw args)]
           :else           [tag nil args])))
 
+(defn- ^{:from 'org.clojure/core.incubator} silent-read
+  "Attempts to clojure.core/read a single form from the provided String, returning
+  a vector containing the read form and a String containing the unread remainder
+  of the provided String. Returns nil if no valid form can be read from the
+  head of the String."
+  [s]
+  (try
+    (let [r (-> s java.io.StringReader. java.io.PushbackReader.)]
+      [(read r) (slurp r)])
+    (catch Exception e))) ; this indicates an invalid form -- the head of s is just string data
+
+(defn- ^{:from 'org.clojure/core.incubator} terpol8*
+  "Yields a seq of Strings and read forms."
+  ([s atom?]
+   (lazy-seq
+     (if-let [[form rest] (silent-read (subs s (if atom? 2 1)))]
+       (cons form (terpol8* (if atom? (subs rest 1) rest)))
+       (cons (subs s 0 2) (terpol8* (subs s 2))))))
+  ([^String s]
+   (if-let [start (->> ["~{" "~("]
+                       (map #(.indexOf s ^String %))
+                       (remove #(== -1 %))
+                       sort
+                       first)]
+     (lazy-seq (cons
+                 (subs s 0 start)
+                 (terpol8* (subs s start) (= \{ (.charAt s (inc start))))))
+     [s])))
+
 (defn terpol8 [s]
-  (let [parts (remove #(= "" %) (#'strint/interpolate s))]
+  (let [parts (remove #(= "" %) (terpol8* s))]
     (if (every? string? parts) s `(str ~@parts))))
 
 (defn flatten-expr-1 [expr sym]
