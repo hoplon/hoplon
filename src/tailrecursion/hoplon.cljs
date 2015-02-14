cc602a93b00658d7ecdc9ba1d9f3baac8d1ebdf6
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-02-15 00:39:31 +0300

    add <main> tag

--- src/tailrecursion/hoplon.cljs
+++ src/tailrecursion/hoplon.cljs
@@ -211,6 +211,7 @@
 (def li             (make-elem-ctor "li"))
 (def link           (make-elem-ctor "link"))
 (def html-map       (make-elem-ctor "map"))
+(def main           (make-elem-ctor "main"))
 (def mark           (make-elem-ctor "mark"))
 (def menu           (make-elem-ctor "menu"))
 (def html-meta      (make-elem-ctor "meta"))
