f65bad0761df4924a11c4263506ff10b9523a58f
David Meister <thedavidmeister@gmail.com>
2016-02-23 22:01:12 +1100

    debug

--- test/hoplon/index_test.clj
+++ test/hoplon/index_test.clj
@@ -1,13 +1,16 @@
 (ns hoplon.index-test
   (:use
     [clojure.test]
-    [clj-webdriver.taxi]))
+    [clj-webdriver.taxi]
+    [clojure.java.shell :only [sh]]))
 
 (set-driver! {:browser :firefox})
-(def base-url (str "file://" (-> (java.io.File. "") .getAbsolutePath) "/target/index.html"))
+(def current-dir (-> (java.io.File. "") .getAbsolutePath))
+(def base-url (str "file://" current-dir "/target/index.html"))
 
 (deftest foo
   (prn base-url)
+  (println (sh "ls" "-la" current-dir))
   (to base-url)
   (is (= "hello world" (text (element "h1"))))
   (quit)
