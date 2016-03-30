0cb02ce4a099b2eb0ee77d57bf03640c5028c2ad
David Meister <thedavidmeister@gmail.com>
2016-03-31 00:18:42 +1100

    leverage fixtures for selenium start/end

--- test/tests/hoplon/index_test.clj
+++ test/tests/hoplon/index_test.clj
@@ -1,13 +1,10 @@
 (ns hoplon.index-test
   (:use
     [clojure.test]
-    [clj-webdriver.taxi]))
+    [clj-webdriver.taxi]
+    [test-util.fixtures :as fixtures]))
 
-(set-driver! {:browser :firefox})
-
-(def base-url "http://localhost:3000")
+(use-fixtures :each fixtures/selenium-driver!)
 
 (deftest first-test
-  (to base-url)
-  (is (= "hello world" (text (element "h1"))))
-  (quit))
+  (is (= "hello world" (text (element "h1")))))
