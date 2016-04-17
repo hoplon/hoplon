d27778c3da6a4b62a610347d2dd4687f7057fa41
David Meister <thedavidmeister@gmail.com>
2016-04-17 17:25:23 +1000

    more tests for dom churn

--- test/tests/hoplon/dom_churn_test.clj
+++ test/tests/hoplon/dom_churn_test.clj
@@ -30,3 +30,29 @@
 
 (deftest add-remove-inputs-same
   (add-remove-inputs! (let [t "a"] [t t t])))
+
+(deftest ^:wip deterministic-loop-tpl
+  (let [inputs                #(elements "#deterministic-loop-tpl input")
+        nth-input             #(nth (inputs) %)
+        nth-value             #(value (nth-input %))
+        nth-value=            #(= %2 (nth-value %1))
+        verify-state-1        #(do  (is (= 2 (count (inputs))))
+                                    (is (nth-value= 0 "a"))
+                                    (is (nth-value= 1 "a")))
+        verify-state-2        #(do  (is (= 1 (count (inputs))))
+                                    (is (nth-value= 0 "a")))
+        state-1!              #(do  (click "#deterministic-loop-tpl [data-state-1]")
+                                    (verify-state-1))
+        state-2!              #(do  (click "#deterministic-loop-tpl [data-state-2]")
+                                    (verify-state-2))
+        toggle-states-a-bit!  #(do  (state-1!)
+                                    (state-2!)
+                                    (state-1!)
+                                    (state-2!))]
+    (is (= 0 (count (inputs))))
+    (toggle-states-a-bit!)
+    (state-1!)
+    (send-keys (nth-input 0) "\b")
+    (toggle-states-a-bit!)
+    (state-1!)
+    (send-keys (nth-input 1) "\b")))
