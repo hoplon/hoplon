9c536139c03787b4e7d2fa517922bb031b6fd8b0
jumblerg <jumblerg@gmail.com>
2016-07-06 16:46:54 -0400

    bump deps, enhance tests

--- build.boot
+++ build.boot
@@ -1,73 +1,46 @@
 (set-env!
-  :source-paths #{"src" "test/tests"}
-  :resource-paths #{"test/src"}
-  ;; using the sonatype repo is sometimes useful when testing Clojurescript
-  ;; versions that not yet propagated to Clojars
-  ;; :repositories #(conj % '["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"}])
-  :dependencies '[[org.clojure/clojure       "1.7.0"      :scope "provided"]
-                  [org.clojure/clojurescript "1.7.122"    :scope "provided"]
-                  [adzerk/boot-cljs          "1.7.48-3"   :scope "test"]
-                  [adzerk/bootlaces          "0.1.10"     :scope "test"]
-                  [hoplon/boot-hoplon        "0.1.13"     :scope "test"]
-                  [adzerk/boot-test          "1.1.1"      :scope "test"]
-                  [clj-webdriver             "0.7.2"      :scope "test"]
-                  [pandeiro/boot-http        "0.7.0"      :scope "test"]
-                  [org.seleniumhq.selenium/selenium-java "2.53.1" :scope "test"]
-                  [cljsjs/jquery             "1.9.1-0"]
-                  [hoplon/javelin            "3.8.4"]])
+  :asset-paths    #{"tst/rsc"}
+  :source-paths   #{"src" "tst/src"}
+  :resource-paths #{"tst/tst"}
+  :dependencies '[[org.clojure/clojure                   "1.7.0"    :scope "provided"]
+                  [org.clojure/clojurescript             "1.7.122"  :scope "provided"]
+                  [adzerk/boot-cljs                      "1.7.48-3" :scope "test"]
+                  [adzerk/bootlaces                      "0.1.13"   :scope "test"]
+                  [hoplon/boot-hoplon                    "0.2.0"    :scope "test"]
+                  [adzerk/boot-reload                    "0.4.11"   :scope "test"]
+                  [adzerk/boot-test                      "1.1.2"    :scope "test"]
+                  [clj-webdriver                         "0.7.2"    :scope "test"]
+                  [tailrecursion/boot-static             "0.1.0"    :scope "test"]
+                  [org.seleniumhq.selenium/selenium-java "2.53.1"   :scope "test"]
+                  [cljsjs/jquery                         "1.9.1-0"]
+                  [hoplon/javelin                        "3.8.4"]])
 
-(require '[adzerk.bootlaces :refer :all]
-         '[hoplon.boot-hoplon :refer [hoplon prerender]]
-         '[adzerk.boot-cljs :refer [cljs]]
-         '[adzerk.boot-test :refer [test]]
-         '[pandeiro.boot-http :refer [serve]])
+(require
+  '[adzerk.bootlaces          :refer :all]
+  '[hoplon.boot-hoplon        :refer [hoplon prerender]]
+  '[adzerk.boot-reload        :refer [reload]]
+  '[adzerk.boot-cljs          :refer [cljs]]
+  '[adzerk.boot-test          :refer [test]]
+  '[tailrecursion.boot-static :refer [serve]])
 
 (def +version+ "6.0.0-alpha16")
 
 (bootlaces! +version+)
 
+(replace-task!
+  [t test] (fn [& xs] (comp (hoplon) (cljs) (serve) (apply t xs))))
+
+(deftask develop-tests []
+  (comp (watch) (speak) (test)))
+
+(deftask develop []
+  (comp (watch) (speak) (build-jar)))
+
 (task-options!
-  pom  {:project     'hoplon
-        :version     +version+
-        :description "Hoplon web development environment."
-        :url         "https://github.com/hoplon/hoplon"
-        :scm         {:url "https://github.com/hoplon/hoplon"}
-        :license     {"Eclipse Public License" "http://www.eclipse.org/legal/epl-v10.html"}})
-
-(def target-dir "target")
-
-(defn test-filter-for-wip
-  [wip?]
-  (if wip?
-      '(:wip (meta %))
-      '(not (:wip (meta %)))))
-
-(deftask webdriver-tests
-  "Run all Selenium + Firefox tests"
-  [ w watch?  bool "Watches the filesystem and reruns tests when changes are made."
-    W wip?    bool "true to only run WIP tests. WIP tests will not run if false."]
-  (comp
-    (target :dir #{target-dir})
-    (serve :dir target-dir)
-    (if watch?
-        (comp
-          (watch)
-          (speak))
-        identity)
-    (hoplon)
-    (cljs)
-    (target :dir #{target-dir})
-    (test
-      :filters [(test-filter-for-wip wip?)])))
-
-(deftask dev
-  "Build Hoplon for local development."
-  []
-  (comp
-    (target :dir #{target-dir})
-    (serve :dir target-dir)
-    (watch)
-    (speak)
-    (hoplon)
-    (cljs)
-    (target :dir #{target-dir})))
+  pom   {:project     'hoplon
+         :version     +version+
+         :description "Hoplon web development environment."
+         :url         "https://github.com/hoplon/hoplon"
+         :scm         {:url "https://github.com/hoplon/hoplon"}
+         :license     {"Eclipse Public License" "http://www.eclipse.org/legal/epl-v10.html"}}
+  serve {:port 3020})
--- tst/src/hoplon/app.cljs
+++ tst/src/hoplon/app.cljs
@@ -1,25 +1,45 @@
-(page "index.html")
+(ns+ hoplon.app
+  (:page
+    "index.html")
+  (:require
+    [javelin.core :refer [cell cell=]]
+    [hoplon.core  :refer :all]))
+
+; (defelem toc
+;   [{:keys [toc]} _]
+;   (div
+;     (h3 "Table of Contents")
+;     (div
+;       (loop-tpl
+;         :bindings [{:keys [num title]} toc]
+;         (p (a :href "#" (text "~{num}...~{title}")))))))
+;
+; (defelem chapter
+;   [{:keys [toc title]} kids]
+;   (let [chapter? #(= :chapter (:type %))
+;         chapters (filter chapter? @toc)
+;         num      (inc (count chapters))
+;         this-toc {:type :chapter :num num :title title}]
+;     (swap! toc conj this-toc)
+;     (section
+;       (h1
+;         (text "Chapter ~{num}: ~{title}"))
+;       kids)))
 
 (html
   (head
-    (link :rel "stylesheet" :href "main.css")
+    (link :rel "stylesheet" :href "app.css")
     (html-meta :charset "UTF-8")
-    (base
-      :href "http://hoplon.io/images/")
+    (base :href "http://hoplon.io/images/")
     (title "Test examples")
-    (style
-      "h1 {color:red};} p {color:blue};}"))
-
+    (style "h1 {color:red};} p {color:blue};}"))
   (body
     (noscript "Your browser does not support JavaScript!")
     (noframes "Sorry, your browser does not handle frames!")
-
     (script "1;")
-
     (nav
       (menu :type "context" :id "mymenu"
         (menuitem :label "Refresh")))
-
     (article
       (header
         (h1 "hello world")
@@ -48,15 +68,12 @@
         :href "http://hoplon.io"
         :alt "click me"))
     (audio :controls true)
-
     (html-object :width "400" :height "400" :data "helloworld.swf"
       (param :name "autoplay" :value "true"))
-
     (br)
     (bdo
       :dir "rtl"
       "Right to " (bdi "left"))
-
     (section
       (blockquote
         :cite "http://hoplon.io"
@@ -68,19 +85,14 @@
         (small "build single-page")
         " web applications. "
         (samp "Learn more on our wiki.")))
-
     (pre
       "Text in a pre element
       is displayed in a fixed-width
       font, and it preserves
       both      spaces and
       line breaks")
-
-    (button
-      :type "button"
-      "click me")
+    (button :type "button" "click me")
     (canvas)
-
     (table
       (caption "Monthly savings")
       (colgroup
@@ -92,7 +104,6 @@
       (tr
         (td "January")
         (td "$100")))
-
     (table
       (thead
         (tr
@@ -106,10 +117,8 @@
         (tr
           (td "January")
           (td "$100"))))
-
     (p  "Can I get "
         "?")
-
     (cite "cite")
     (code "(= (+ 1 1) 2)")
     (input :list "numbers")
@@ -128,70 +137,53 @@
       (dd "Black hot drink")
       (dt "Milk")
       (dd "White cold drink"))
-
     (main
       (p "My favorite color is " (del "blue") (ins "red") "!")
-
       (details
         (summary "Copyright 1999-2014.")
         (p " - by Refsnes Data. All Rights Reserved.")
         (p "All content and " (s "graphics" ) " on this web site are the property of the company Refsnes Data."))
-
       (div (dfn "HTML") " is the " (em "standard markup") "language for creating web pages.")
-
       (p "He named his car " (i "The lightning") ", because it was very fast.")
-
       (p  "Do not "
           (q "forget to buy ")
           (mark "milk")
           " today."))
-
     (ruby
       "漢"
       (rp "(")
       (rt "ㄏㄢˋ")
       (rtc "San Francisco")
       (rp ")"))
-
     (meter :value "2" :min "0" :max "10" "2 out of 10")
     (progress :value "22" :max "100")
-
     (embed :src "foo.swf")
-
     (video :width "320" :height "240" :controls true
       (source :src "movie.mp4" :type "video/mp4")
       (source :src "movie.ogg" :type "video/ogg")
       (track :src "subtitles_no.vtt" :kind "subtitles" :srclang "no" :label "Norwegian")
       "Your browser does not support the video tag.")
-
     (p  "To learn AJAX, you must be familiar with the XML"
         (wbr "Http")
         "Request Object.")
-
     (dialog :open true
       (p "Greetings, one and all!"))
-
     (form
       (fieldset
         (legend "Personalia:")
         "Name: " (input :type "text")
         "Email: " (input :type "text")
         "Date of birth: " (input :type "text")))
-
     (iframe :src "http://hoplon.io")
-
     (ol
       (li "Coffee")
       (li "Tea")
       (li "Milk"))
-
     (ul
       (li "Coffee")
       (li "Tea")
       (li "Milk"))
-
     (html-var "Variable")
-
     (p "New " (u "Products"))
     (ul
       (li
@@ -200,9 +192,7 @@
         (data :value "3967381399" "Jumbo Ketchup"))
       (li
         (data :value "3967381400" "Mega Jumbo Ketchup")))
-
     (p "We open at " (html-time "10:00") " every morning.")
-
     (select
       (optgroup :label "Swedish Cars"
         (option :value "volvo" "Volvo")
@@ -211,7 +201,6 @@
       (optgroup :label "German Cars"
         (option :value "mercedes" "Mercedes")
         (option :value "audi" "Audi")))
-
     (let
       [ a (cell 10)
         b (cell 20)]
@@ -221,6 +210,5 @@
         (input :id "b" :value b :input #(reset! b (int @%)))
         "="
         (output :for "a b" :value (cell= (+ a b)))))
-
     (footer
       (p "Press " (kbd "cmd")))))
