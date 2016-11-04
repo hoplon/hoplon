7de013100258322ac86289641a63f2e43d9e35a1
David Meister <thedavidmeister@gmail.com>
2016-11-04 16:59:54 +1100

    remove unneccessary async in on-test

--- tst/cljs/hoplon/on_test.cljs
+++ tst/cljs/hoplon/on_test.cljs
@@ -1,15 +1,13 @@
 (ns hoplon.on-test
-  (:require [cljs.test :refer-macros [deftest is async]]
+  (:require [cljs.test :refer-macros [deftest is]]
             [hoplon.core :as h]
             [javelin.core :as j]))
 
 (deftest bind-events
-  (async done
-    (let [c (j/cell nil)
-          f #(reset! c true)
-          i (h/div :click f)
-          e (js/Event. "click")]
-      (is (nil? @c))
-      (.dispatchEvent i e)
-      (h/with-timeout 0 (do (is (= true @c))
-                            (done))))))
+  (let [c (j/cell nil)
+        f #(reset! c true)
+        i (h/div :click f)
+        e (js/Event. "click")]
+    (is (nil? @c))
+    (.dispatchEvent i e)
+    (is (= true @c))))
