8d51438fdad0309d953fdc2b3cdc670a2983f666
David Meister <thedavidmeister@gmail.com>
2017-07-09 19:04:36 +1000

    additional tests from flyboarder

--- tst/src/cljs/hoplon/sort_test.cljs
+++ tst/src/cljs/hoplon/sort_test.cljs
@@ -13,8 +13,8 @@
             (h/for-tpl [[k v] sorted-data]
              (h/input
               :data-k k
-              ; :value [v data]
-              :value v
+              :value [v data]
+              ; :value v
               :input #(swap! data assoc @k (int @%)))))
         read-vals (fn [el]
                    (map
@@ -34,4 +34,27 @@
     (is (= {:a 3 :b 1 :c 2} @data)) ; passes
     (is (= [":b" ":c" ":a"] (read-ks el))) ; passes
     (is (= ["1" "2" "3"] (read-vals el))) ; fails with ["3" "2" "3"]!
+    (done)))))
+
+(deftest ??sorting-more
+ (async done
+  (let [data (j/cell ["a" "a" "b"])
+        sdata (j/cell= (sort data))
+        idata (j/cell= (into {} (map-indexed vector sdata)))
+        rdata (j/cell= idata #(reset! data (vals %)))
+        el (h/div
+            (h/for-tpl [[k v] rdata]
+             (h/input :change #(swap! rdata assoc @k @%) :value v)))
+        read-vals (fn [el]
+                   (map
+                    #(-> % js/jQuery .val)
+                    (-> el js/jQuery (.find "input") array-seq)))]
+
+   (-> js/document .-body (.appendChild el))
+
+   (h/with-dom el
+    (is (= @sdata (read-vals el) ["a" "a" "b"]))
+
+    (-> el js/jQuery (.find "input") .first (.val "c") (.trigger "change"))
+    (is (= @sdata (read-vals el) ["a" "b" "c"]))
     (done)))))
