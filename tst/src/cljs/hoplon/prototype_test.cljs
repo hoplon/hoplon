d5945f105fd5a9388f0a371b11003a7a61eaf303
David Meister <thedavidmeister@gmail.com>
2017-12-23 13:18:11 +1100

    extend ??appendChild

--- tst/src/cljs/hoplon/prototype_test.cljs
+++ tst/src/cljs/hoplon/prototype_test.cljs
@@ -14,13 +14,27 @@
                          [(.createElement js/document "head")
                           (.createComment js/document "foo")]
                          [(.createElement js/document "head")
-                          (.createElement js/document "div")]]]
+                          (.createElement js/document "div")]
+                         [(.createElement js/document "body")
+                          (h/$text "foo")]]]
   (.appendChild parent child)
   (is (= parent (.-parentNode child)))
   (doseq [n [child parent]]
    (when (instance? js/Element n)
     (h/native? n))
    (is (h/native-node? n))
-   (is (not (h/managed? n))))))
+   (is (not (h/managed? n)))))
 
- ; if any node is managed
+ ; if the parent is native, but the child is managed but not a cell then the
+ ; parent continues to be native and child continues as managed.
+ (doseq [[parent child] [[(.createElement js/document "div")
+                          (h/div)]]]
+  (.appendChild parent child)
+  (is (= parent (.-parentNode child)))
+  (is (h/native? parent))
+  (is (h/native-node? parent))
+  (is (not (h/managed? parent)))
+
+  (is (not (h/native? child)))
+  (is (not (h/native-node? child)))
+  (is (h/managed? child))))
