6ee4179b40a38fa27df7c86cb94faf96a6a0459c
David Meister <thedavidmeister@gmail.com>
2017-12-23 13:13:19 +1100

    extend ??appendChild

--- tst/src/cljs/hoplon/prototype_test.cljs
+++ tst/src/cljs/hoplon/prototype_test.cljs
@@ -5,11 +5,22 @@
 
 (deftest ??appendChild
  ; if all involved nodes are native, there should be no hoplon management
- (let [child (.createElement js/document "div")
-       parent (.createElement js/document "div")]
+ (doseq [[parent child] [[(.createElement js/document "div")
+                          (.createElement js/document "div")]
+                         [(.createElement js/document "body")
+                          (.createTextNode js/document "foo")]
+                         [(.createElement js/document "body")
+                          (.createElement js/document "div")]
+                         [(.createElement js/document "head")
+                          (.createComment js/document "foo")]
+                         [(.createElement js/document "head")
+                          (.createElement js/document "div")]]]
   (.appendChild parent child)
   (is (= parent (.-parentNode child)))
   (doseq [n [child parent]]
-   (is (h/native? n))
+   (when (instance? js/Element n)
+    (h/native? n))
    (is (h/native-node? n))
    (is (not (h/managed? n))))))
+
+ ; if any node is managed
