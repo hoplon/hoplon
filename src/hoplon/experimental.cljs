920dbc66d09ae7ef47c32f9477b8347ca7d76135
Matthew Ratzke <matt@degree9.io>
2017-12-25 23:31:25 -0700

    move static-elements

--- src/hoplon/experimental.cljs
+++ src/hoplon/experimental.cljs
@@ -1,6 +1,12 @@
 (ns hoplon.experimental
   (:include-macros [hoplon.experimental :refer [cache-key]]))
 
+(def ^:no-doc static-elements
+  "This is an internal implementation detail, exposed for the convenience of
+  the hoplon.core/static macro. Experimental."
+  (-> #(assoc %1 (.getAttribute %2 "static-id") %2)
+      (reduce {} (.querySelector js/document "[static-id]"))))
+
 (defn bust-cache
   "Public helper.
   Experimental."
