9e8da950e498e5eae47cde215dabd36f66e8b369
David Meister <thedavidmeister@gmail.com>
2018-01-18 15:04:41 +1100

    remove goog.dom dep and implement IFn for DocumentFragment

--- src/hoplon/core.cljs
+++ src/hoplon/core.cljs
@@ -15,8 +15,7 @@
             [clojure.string       :refer [split join blank?]]
             [cljs.spec.alpha      :as spec]
             [cljs.spec.test.alpha :as spect]
-            [hoplon.spec]
-            [goog.dom :as gdom])
+            [hoplon.spec])
   (:require-macros [javelin.core :refer [with-let cell= prop-cell]]
                    [hoplon.core  :refer [with-timeout with-dom]]))
 
@@ -412,6 +411,55 @@
      (invoke! this a b c d e f g h i j k l m n o p q r s t))
     ([this a b c d e f g h i j k l m n o p q r s t rest]
      (invoke! this a b c d e f g h i j k l m n o p q r s t rest))))
+
+(extend-type js/DocumentFragment
+  ; a direct copy of IFn for Element
+  IFn
+  (-invoke
+    ([this]
+     (invoke! this))
+    ([this a]
+     (invoke! this a))
+    ([this a b]
+     (invoke! this a b))
+    ([this a b c]
+     (invoke! this a b c))
+    ([this a b c d]
+     (invoke! this a b c d))
+    ([this a b c d e]
+     (invoke! this a b c d e))
+    ([this a b c d e f]
+     (invoke! this a b c d e f))
+    ([this a b c d e f g]
+     (invoke! this a b c d e f g))
+    ([this a b c d e f g h]
+     (invoke! this a b c d e f g h))
+    ([this a b c d e f g h i]
+     (invoke! this a b c d e f g h i))
+    ([this a b c d e f g h i j]
+     (invoke! this a b c d e f g h i j))
+    ([this a b c d e f g h i j k]
+     (invoke! this a b c d e f g h i j k))
+    ([this a b c d e f g h i j k l]
+     (invoke! this a b c d e f g h i j k l))
+    ([this a b c d e f g h i j k l m]
+     (invoke! this a b c d e f g h i j k l m))
+    ([this a b c d e f g h i j k l m n]
+     (invoke! this a b c d e f g h i j k l m n))
+    ([this a b c d e f g h i j k l m n o]
+     (invoke! this a b c d e f g h i j k l m n o))
+    ([this a b c d e f g h i j k l m n o p]
+     (invoke! this a b c d e f g h i j k l m n o p))
+    ([this a b c d e f g h i j k l m n o p q]
+     (invoke! this a b c d e f g h i j k l m n o p q))
+    ([this a b c d e f g h i j k l m n o p q r]
+     (invoke! this a b c d e f g h i j k l m n o p q r))
+    ([this a b c d e f g h i j k l m n o p q r s]
+     (invoke! this a b c d e f g h i j k l m n o p q r s))
+    ([this a b c d e f g h i j k l m n o p q r s t]
+     (invoke! this a b c d e f g h i j k l m n o p q r s t))
+    ([this a b c d e f g h i j k l m n o p q r s t rest]
+     (invoke! this a b c d e f g h i j k l m n o p q r s t rest))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; HTML Constructors ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -664,28 +712,29 @@
 ;; elements in the new order seems to work, so that's what I'm going
 ;; with. Need to remember to talk to Micha about this.
 (defn keyed-loop-tpl*
-  "Like `loop-tpl*` but accepts a `key-fn` which, given a list item returns an
-  (immutable) key under which to cache and reuse the rendered DOM element."
-  [items key-fn tpl]
-  (let [pos-map (hoplon.core/formula-of [items]
-                  (zipmap (map key-fn items)
-                          (range)))
-        index (atom {})]
-    (with-let [current (cell [])]
-      (do-watch
-       pos-map
-       (fn [_ _]
-         (reset! current
-                 (with-let [frag (.createDocumentFragment js/document)]
-                   (doseq [item @items]
-                     (let [k (key-fn item)]
-                       (when-not (contains? @index k)
-                         (swap! index
-                                assoc
-                                k
-                                {:item item
-                                 :ui (tpl
-                                      (hoplon.core/formula-of [items pos-map]
-                                        (when-let [pos (get pos-map k)]
-                                          (nth items pos nil))))}))
-                       (gdom/appendChild frag (get-in @index [k :ui])))))))))))
+ "Like `loop-tpl*` but accepts a `key-fn` which, given a list item returns an
+ (immutable) key under which to cache and reuse the rendered DOM element."
+ [items key-fn tpl]
+ (let [pos-map (hoplon.core/formula-of [items]
+                 (zipmap (map key-fn items)
+                         (range)))
+       index (atom {})]
+  (with-let [current (cell [])]
+   (do-watch
+    pos-map
+    (fn [_ _]
+     (reset! current
+      (with-let [frag (.createDocumentFragment js/document)]
+       (doseq [item @items]
+        (let [k (key-fn item)]
+         (when-not (contains? @index k)
+          (swap!
+           index
+           assoc
+           k
+           {:item item
+            :ui (tpl
+                 (hoplon.core/formula-of [items pos-map]
+                  (when-let [pos (get pos-map k)]
+                   (nth items pos nil))))}))
+         (frag (get-in @index [k :ui])))))))))))
