d502e3dd0370b1d86dafca6153a3aa94a3056fb5
David Meister <thedavidmeister@gmail.com>
2019-07-30 00:45:19 +1000

    tests for check-val!

--- tst/src/cljs/hoplon/jquery_test.cljs
+++ tst/src/cljs/hoplon/jquery_test.cljs
@@ -1,5 +1,7 @@
 (ns hoplon.jquery-test
  (:require
+  [hoplon.core :as h]
+  [javelin.core :as j]
   hoplon.jquery
   hoplon.svg
   [cljs.test :refer-macros [deftest is]]))
@@ -13,3 +15,43 @@
  (let [el (hoplon.svg/svg :svg/viewBox "0 0 10 10")]
   (is (= "0 0 10 10" (.getAttribute el "viewBox")))
   (is (nil? (.getAttribute el "viewbox")))))
+
+(deftest ??check-val!
+ (let [c (j/cell nil)
+       checkbox (h/input :value c :type "checkbox")
+       radio (h/input :value c :type "radio")]
+  (doseq [el [checkbox radio]]
+   ; try a false value
+   (reset! c false)
+   (is
+    (not (.-checked el))
+    "native is false when false")
+   (is
+    (not (:checked el))
+    ":checked is false when false")
+   (is
+    (not (:value el))
+    ":value is false when false")
+   ; try a true value
+   (reset! c true)
+   (is
+    (.-checked el)
+    "native is true when true")
+   ; @TODO implement :checked and :value lookups for jquery elems
+   ; (is
+   ;  (:checked el)
+   ;  ":checked is true when true")
+   ; (is
+   ;  (:value el)
+   ;  ":value is true when true")
+   ; try reverting to nil
+   (reset! c nil)
+   (is
+    (not (.-checked el))
+    "native is false when nil")
+   (is
+    (not (:checked el))
+    ":checked is false when nil")
+   (is
+    (not (:value el))
+    ":value is false when nil"))))
